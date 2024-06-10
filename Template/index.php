d6aeed73f9be2b91adaa1724b47d6181bc742cb2
flavien <flavioumberto1@gmail.com>
2024-06-10 20:20:48 +0200

    adding template folder, index.php ,style.css

---
+++ Template/index.php
@@ -0,0 +1,81 @@
+<?php 
+
+
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Formulaire de contact avancé </title>
+</head>
+<body>
+   <div class="mainContainer">
+
+    <div class="titleContainer">
+        <h2 class ="titleContainer_title">Formulaire de contact avancé</h2>
+    </div>
+
+    <div class="formAndListContainer">
+
+        <div class="Formulaire">
+            <h4>Formulaire</h4>
+        <form action="" method="post" class="form">
+
+           <div class="input_name">
+                <label for="" class="label">Name</label>
+                <input type="text" name="name">
+           </div>
+
+            <div class="input_email">
+                <label for="" class="label">Email</label>
+                <input type="text" name="email">
+            </div>
+
+           <div class="input_countries">
+
+        <label for="" class="label">Selectionnez votre pays de residence</label>
+        <select name="countries" id="">
+            <option value=" Democratic republic of congo"> Democratic republic of congo</option>
+            <option value=" Republic of zambia"> Republic of zambia </option>
+            <option value="Republic of namibia">Republic of namibia</option>
+  
+        </select>
+           </div>
+
+       <div class="input_newsLetter">
+            <label for="" class="label">Cliquez sur le buton ci-dessous pour vous abonnez à la news letter</label>
+            <input type="checkbox" name="newsLetter" id="">
+       </div>
+       <div  class="input_gender">
+      <label class="label" for="">de quel genre etes vous ?</label>
+
+        <div class="input_gender_Masculin">
+        <span>Masculin</span>
+        <input type="radio" name="Masculin" id="">
+        </div>
+
+        <div class="input_gender_Feminin">
+          <span>Feminin</span>
+          <input type="radio" name="Feminin" id="">
+        </div>
+
+       </div>
+  
+        <div  class="input_upload">
+            <label for="" class="label">Téléchargez votre photo de profil</label>
+            <input type="file" name="upload" id="">
+        </div>
+
+        <button type="submit">Submit</button>
+    </form>
+        </div>
+    </div>
+   </div>
+</body>
+</html>
