8eb39d858c0173c67fc394c1c93fc224f1ae270f
flavien <flavioumberto1@gmail.com>
2024-06-20 16:23:18 +0200

    mise en place de l'auto loader et rassembement des classes dans le dossiers Classe

---
+++ CRUD/Classe/HTMLElement.php
@@ -0,0 +1,41 @@
+<?php 
+
+class HTMLElement{
+ 
+    protected string $tagName;
+    protected array $attribut = [];
+    protected string $content =" ";
+
+    public function __construct( $tagName,$content ){
+
+        $this->tagName= $tagName;
+        $this->content= $content;
+
+    }
+
+    public function setAttribut($name,$valeur){
+
+      $this->attribut[$name]= $valeur;
+ 
+    }
+     public function getAttribut($name){
+
+        return isset($this->attribut[$name]) ? $this->attribut[$name] : null;
+     
+    }
+
+    public function render() {
+
+        $attrs = "";
+        foreach ($this->attribut as $name => $value) {
+            $attrs .= " $name=\"$value\"";
+        }
+
+        return "<{$this->tagName}{$attrs}>{$this->content}</{$this->tagName}>";
+    }
+
+
+}
+
+?>
+
